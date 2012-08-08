4849b2df29208be3e8529b8741e192f1e005220c
prasanna <prasannaraajkumar@gmail.com>
2012-08-08 15:28:30 -0700

    login added again

--- client/js/login.js
+++ client/js/login.js
@@ -8,21 +8,9 @@
 
 var VC = {"domain" : "@vmware.com"};
 
-//todo REMOVE THIS
-//VC.cloudFoundryUrl = "http://vcommute.cloudfoundry.com:80";
-//var href = document.location.href;
-//VC.url = VC.cloudFoundryUrl;
-//if(href.indexOf("cloudfoundry") == -1 || href.indexOf("file") != -1)  {
-//    VC.url = document.location.host;
-//}
-//VC.url = "http://" + document.location.host;
-
-
 VC.url = "http://vcommute.cloudfoundry.com:80";
 //VC.url = "http://" + document.location.host;
 VC.socket = io.connect(VC.url);
-
-
 
 var USERSTATUS = {
     NEWUSER:"NEW USER",
